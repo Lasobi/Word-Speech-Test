49079eb65fa8598e8534ff3ec4b6b475a862df8a
Lars S Birkeland <30419055+Lasobi@users.noreply.github.com>
2022-12-04 01:52:28 +0100

    some background changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Word-Speech-Test',
+    version='0.3',
+    packages=[''],
+    url='https://github.com/Lasobi/Word-Speech-Test',
+    license='MIT',
+    author='Lars Birkeland',
+    author_email='30419055+Lasobi@users.noreply.github.com',
+    description='A tool to test pronunciation of words using Whisper to check if the word is understandable.'
+)
